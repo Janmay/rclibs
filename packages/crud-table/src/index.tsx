63112ac96b71aae04cbc39fb540e5b60674a2a19
jane <learningweb.shen@outlook.com>
2020-06-29 17:08:47 +0800

    feat: fetch table data(uncompleted)

--- packages/crud-table/src/index.tsx
+++ packages/crud-table/src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import {Table} from 'antd';
 import {ColumnsType, TableProps} from 'antd/es/table';
 import {RequestData} from './useFetchData';
@@ -6,12 +6,7 @@ import {RequestData} from './useFetchData';
 export interface CrudTableProps<T, U extends Record<string, any>>
   extends TableProps<T> {
   params?: U;
-  request?: (
-    params: U & {
-      pageSize?: number;
-      current?: number;
-    },
-  ) => Promise<RequestData<T>>;
+  request?: RequestData<T, U>;
 }
 
 const CrudTable = <
@@ -20,7 +15,15 @@ const CrudTable = <
 >(
   props: CrudTableProps<T, U>,
 ) => {
-  const {request} = props;
+  const {
+    request,
+    params = {},
+    pagination: propPagination,
+    columns: propColumns = [],
+    ...rest
+  } = props;
+
+  const [dataSource, setDataSource] = useState<T[]>([]);
 
   return <div></div>;
 };
--- packages/crud-table/src/useFetchData.ts
+++ packages/crud-table/src/useFetchData.ts
@@ -1,3 +1,35 @@
-export interface RequestData<T> extends Record<string, any> {
+import {useState} from 'react';
+
+export interface DataResult<T> extends Record<string, any> {
   data: T[];
 }
+
+export interface RequestData<T, U extends Record<string, any>> {
+  (
+    params: U & {
+      pageSize?: number;
+      current?: number;
+    },
+  ): Promise<DataResult<T>>;
+}
+
+export interface UseFetchDataAction<T> {
+  dataSource: T[];
+  loading?: boolean;
+}
+
+const useFetchData = <T>(
+  getData: RequestData<T, Record<string, any>>,
+  defaultData?: T[],
+  options?: {
+    defaultCurrent?: number;
+    defaultPageSize?: number;
+    onLoad?: (dataSource: T[]) => void;
+    onError?: (e: Error) => void;
+  },
+): UseFetchDataAction<T> => {
+  const [dataSource, setDataSource] = useState<T[]>(defaultData);
+  return {
+    dataSource,
+  };
+};
